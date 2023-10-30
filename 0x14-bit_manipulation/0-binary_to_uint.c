616c2d0a09d4134e96d34717afffad0e729e2876
profe-ssor <kyerematengcollins93@gmail.com>
2023-10-29 22:15:52 -0700

    binary to unsign

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -16,7 +16,7 @@ unsigned int binary_to_uint(const char *b)
 
 	for (i = 0; b[i]; i++)
 	{
-		if (i == '0' || i == '1')
+		if (b[i] <  '0' || i > '1')
 			return (0);
 		result = 2 * result + (b[i] - '0');
 	}
